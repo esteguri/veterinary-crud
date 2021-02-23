22a1b6b35123fb14592ca4f5354e578447531f30
Esteban Gutierrez <egutierrez@tsgroup.com.co>
2021-02-22 22:33:02 -0500

    Veterinary CRUD in memory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,87 @@
-import { faEdit, faInfoCircle, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faEnvelope, faInfoCircle, faMapMarkerAlt, faPhone, faPlus, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import {React, useState} from 'react'
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [type, setType] = useState('')
+  const [birthDate, setBirthDate] = useState('')
+  const [breed, setBreed] = useState('')
+  const [ownerName, setOwnerName] = useState('')
+  const [tel, setTel] = useState('')
+  const [address, setAddress] = useState('')
+  const [email, setEmail] = useState('')
+  const [patients, setPatients] = useState([])
+  const [idSelected, setIdSelected] = useState('')
+  const [isEdit, setIsEdit] = useState(false)
+
+  const addPatient = () => {
+    const patient = {
+      id: Date.now().toString(),
+      name,
+      type,
+      birthDate,
+      breed, 
+      ownerName, 
+      tel, 
+      address, 
+      email
+    }
+    setPatients([...patients, patient])
+    clearFields()
+  }
+
+  const updatePatient = (id) => {
+    const patient = {
+      id,
+      name,
+      type,
+      birthDate,
+      breed, 
+      ownerName, 
+      tel, 
+      address, 
+      email
+    }
+    const newPatients = patients.map(p => p.id == id ? patient : p)
+    setPatients(newPatients)
+    clearFields()
+  }
+
+  const deletePatient = (id) => {
+    const newPatients = patients.filter(p => p.id != id)
+    setPatients(newPatients)
+  }
+
+  const clearFields = () => {
+    setName('')
+    setType('')
+    setBirthDate('')
+    setBreed('')
+    setOwnerName('')
+    setTel('')
+    setAddress('')
+    setEmail('')
+    setIsEdit(false)
+  }
+
+  const setInfoPatient = (patient, isEdit = false) => {
+    setIdSelected(patient.id)
+    setName(patient.name)
+    setType(patient.type)
+    setBirthDate(patient.birthDate)
+    setBreed(patient.breed)
+    setOwnerName(patient.ownerName)
+    setTel(patient.tel)
+    setAddress(patient.address)
+    setEmail(patient.email)
+    setIsEdit(isEdit)
+  }
+
   return (
     <div className="pb-5">
       <Navbar/>
@@ -15,101 +91,167 @@ function App() {
             <h4>Registro de pacientes</h4>
           </div>
           <div className="col-2">
-            <button className="btn btn-success float-end"><FontAwesomeIcon icon={faPlus} /><span className="p-2">Registrar</span></button>
+            <button className="btn btn-outline-success float-end" data-bs-toggle="modal" data-bs-target="#registerModal" onClick={clearFields}>
+              <FontAwesomeIcon icon={faPlus} /><span className="p-2">Registrar</span>
+            </button>
           </div>
         </div>
-        <table class="table table-striped mt-4">
-          <thead>
-            <tr>
-              <th scope="col">Nombre</th>
-              <th scope="col">Tipo</th>
-              <th scope="col">Raza</th>
-              <th scope="col">Fecha de Nacimiento</th>
-              <th scope="col">Propietario</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Yensi</th>
-              <td>Perro</td>
-              <td>Criollo</td>
-              <td>22/01/2010</td>
-              <td>Esteban Gutierrez</td>
-              <td>
-                <button className="btn btn-outline-dark m-1" data-bs-toggle="modal" data-bs-target="#registerModal"><FontAwesomeIcon icon={faEdit}/></button>
-                <button className="btn btn-outline-danger m-1" ><FontAwesomeIcon icon={faTrash}/></button>
-                <button className="btn btn-secondary m-1" data-bs-toggle="modal" data-bs-target="#registerModal"><FontAwesomeIcon icon={faInfoCircle}/> Detalles</button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Yensi</th>
-              <td>Perro</td>
-              <td>Criollo</td>
-              <td>22/01/2010</td>
-              <td>Esteban Gutierrez</td>
-              <td>
-                <button className="btn btn-outline-dark m-1"><FontAwesomeIcon icon={faEdit}/></button>
-                <button className="btn btn-outline-danger m-1"><FontAwesomeIcon icon={faTrash}/></button>
-                <button className="btn btn-secondary m-1"><FontAwesomeIcon icon={faInfoCircle}/> Detalles</button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Yensi</th>
-              <td>Perro</td>
-              <td>Criollo</td>
-              <td>22/01/2010</td>
-              <td>Esteban Gutierrez</td>
-              <td>
-                <button className="btn btn-outline-dark m-1"><FontAwesomeIcon icon={faEdit}/></button>
-                <button className="btn btn-outline-danger m-1"><FontAwesomeIcon icon={faTrash}/></button>
-                <button className="btn btn-secondary m-1"><FontAwesomeIcon icon={faInfoCircle}/> Detalles</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
         
+        {
+          patients.length > 0 ? (
+            <table className="table table-striped mt-4">
+              <thead>
+                <tr>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Tipo</th>
+                  <th scope="col">Raza</th>
+                  <th scope="col">Fecha de Nacimiento</th>
+                  <th scope="col">Propietario</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  patients.map(p => (
+                    <tr key={p.id}>
+                      <th scope="row">{p.name}</th>
+                      <td>{p.type}</td>
+                      <td>{p.breed}</td>
+                      <td>{p.birthDate}</td>
+                      <td>{p.ownerName}</td>
+                      <td>
+                        <button className="btn btn-outline-dark m-1" data-bs-toggle="modal" data-bs-target="#registerModal" onClick={()=> setInfoPatient(p, true)}><FontAwesomeIcon icon={faEdit}/></button>
+                        <button className="btn btn-outline-danger m-1" data-bs-toggle="modal" data-bs-target="#confirmModal" onClick={()=> setInfoPatient(p)}><FontAwesomeIcon icon={faTrash}/></button>
+                        <button className="btn btn-secondary m-1" data-bs-toggle="modal" data-bs-target="#detailsModal" onClick={()=> setInfoPatient(p)}><FontAwesomeIcon icon={faInfoCircle}/> Detalles</button>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          ) : 
+          (
+            <div className="div">
+              <h5 className=" text-center">No hay pacientes</h5>
+              <center><img srcSet={"./empty.png"} width="40%" /></center>
+            </div>
+          )
+        }
       </div>
       <Footer/>
       
+      <div className="modal fade " id="registerModal" aria-labelledby="registerModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-xl">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="registerModalLabel">{isEdit ? 'Editar' : 'Registrar'} paciente</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form className="row g-3 needs-validation mt-2" noValidate>
 
-      <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+                <div className="col-lg-6 col-md-12 row mb-1">
+                  <h5 className="text-success">Datos del paciente</h5>
+                  <div className="col-12">
+                    <label htmlFor="name" className="form-label">Nombre de la mascota:</label>
+                    <input type="text" className="form-control" id="name" placeholder="Nombre de la mascota..." onChange={(text)=>setName(text.target.value)} value={name} ></input>
+                  </div>
+                  <div className="col-6">
+                    <label htmlFor="type" className="form-label">Tipo:</label>
+                    <select className="form-select" id="type" onChange={(text)=>setType(text.target.value)} value={type} >
+                      <option defaultValue >Seleccione...</option>
+                      <option value="Perro">Perro</option>
+                      <option value="Gato">Gato</option>
+                      <option value="Conejo">Conejo</option>
+                      <option value="Ave">Ave</option>
+                      <option value="Otro">Otro</option>
+                    </select>
+                  </div>
+                  <div className="col-6">
+                    <label htmlFor="birthDate" className="form-label">Fecha de nacimiento:</label>
+                    <input type="date" className="form-control" id="birthDate" onChange={(text)=>setBirthDate(text.target.value)} value={birthDate}></input>
+                  </div>
+                  <div className="col-12">
+                    <label htmlFor="breed" className="form-label">Raza:</label>
+                    <input type="text" className="form-control" id="breed"  placeholder="Raza de la mascota..." onChange={(text)=>setBreed(text.target.value)} value={breed}></input>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-md-12 row mb-1">
+                  <h5 className="text-success">Datos del propietario</h5>
+                  <div className="col-12 mt-2">
+                    <label htmlFor="ownerName" className="form-label">Nombres y apellidos:</label>
+                    <input type="text" className="form-control" id="ownerName" placeholder="Nombre del propietario..." onChange={(text)=>setOwnerName(text.target.value)} value={ownerName}></input>
+                  </div>
+                  <div className="col-12 mt-2">
+                    <label htmlFor="tel" className="form-label">Telefono:</label>
+                    <input type="text" className="form-control" id="tel" placeholder="Telefono"  onChange={(text)=>setTel(text.target.value)} value={tel}></input>
+                  </div>
+                  <div className="col-12 mt-2">
+                    <label htmlFor="address" className="form-label">Dirección de residencia:</label>
+                    <input type="address" className="form-control" id="address" placeholder="Dirección" onChange={(text)=>setAddress(text.target.value)} value={address}></input>
+                  </div>
+                  <div className="col-12 mt-2">
+                    <label htmlFor="email" className="form-label">Correo:</label>
+                    <input type="email" className="form-control" id="email" placeholder="Correo" onChange={(text)=>setEmail(text.target.value)} value={email}></input>
+                  </div>
+                </div>
+                
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="button" className="btn btn-success" data-bs-dismiss="modal" onClick={() => isEdit ? updatePatient(idSelected) :  addPatient()}>{isEdit ? 'Editar' : 'Registrar'}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="modal fade " id="detailsModal" aria-labelledby="detailsModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title text-success" id="detailsModalLabel"><FontAwesomeIcon icon={faInfoCircle}/> Detalles <span className="fw-bold">{name}</span></h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="row mt-2">
+                <div className="col-4"><p className="fw-bold"><FontAwesomeIcon icon={faUser} /> Propietario:</p></div>
+                <div className="col-8"><p>{ownerName}</p></div>
+              </div>
+              <div className="row mt-2">
+                <div className="col-4"><p className="fw-bold"><FontAwesomeIcon icon={faPhone} /> Teléfono:</p></div>
+                <div className="col-8"><p>{tel}</p></div>
+              </div>
+              <div className="row mt-2">
+                <div className="col-4"><p className="fw-bold"><FontAwesomeIcon icon={faMapMarkerAlt} /> Dirección:</p></div>
+                <div className="col-8"><p>{address}</p></div>
+              </div>
+              <div className="row mt-2 p-1">
+                <div className="col-4"><p className="fw-bold"><FontAwesomeIcon icon={faEnvelope} /> Dirección:</p></div>
+                <div className="col-8"><p>{email}</p></div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-success" data-bs-dismiss="modal">Aceptar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="registerModalLabel">Registrar paciente</h5>
+              <h5 class="modal-title" id="confirmModalLabel">Confirmar</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <form class="row g-3 needs-validation" novalidate>
-                <div>
-                  <label for="name" class="form-label">Nombre:</label>
-                  <input type="text" class="form-control" id="name" placeholder="Nombre de la mascota..." required></input>
-                </div>
-                <div>
-                  <label for="type" class="form-label">Tipo:</label>
-                  <select class="form-select" id="type">
-                    <option selected >Seleccione un valor...</option>
-                    <option value="perro">Perro</option>
-                    <option value="gato">Gato</option>
-                    <option value="conejo">Conejo</option>
-                    <option value="ave">Ave</option>
-                    <option value="otro">Otro</option>
-                  </select>
-                </div>
-                <div>
-                  <label for="breed" class="form-label">Raza:</label>
-                  <input type="text" class="form-control" id="breed" placeholder="Raza de la mascota..." required></input>
-                </div>
-                <div>
-                  <label for="date" class="form-label">Fecha de nacimiento:</label>
-                  <input type="date" class="form-control" id="date" required></input>
-                </div>
-              </form>
+              ¿Seguro que desea eliminar el paciente <span className="fw-bold">{name}</span>?
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-success">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onClick={()=> deletePatient(idSelected)}>Aceptar</button>
             </div>
           </div>
         </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog, faPaw } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
       <div className="container-fluid bg-success text-white p-3">
-          <div className="display-4 text-center">Veterinaria</div>
+          <div className="display-4 text-center"> <FontAwesomeIcon icon={faPaw} className="p-2"/>Veterinaria</div>
       </div>
   );
 }
